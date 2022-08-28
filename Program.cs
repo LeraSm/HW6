2f54f5a50290462733a15c218a16897097e398d3
Lera <badasyan.lera@mail.ru>
2022-08-28 21:27:42 +0300

    added desicion for task 41

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Console.WriteLine("Hello, World!");
+Console.Clear();
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.Write("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] Arraynumbers= new int [size];
+int count = 0;
+for (int i = 0; i < Arraynumbers.Length; i++)
+{
+    Console.WriteLine($"Введите {i+1}");
+    Arraynumbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < Arraynumbers.Length; i++)
+{
+    if(Arraynumbers[i]>0)
+    {
+        count= count+1;
+    }
+}
+Console.WriteLine($"Количество чисел являющихся больше нуля = {count}");
